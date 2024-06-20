bc9b7bfa3a1a6c24e9145b49e716fe3a6d0e62a6
BryceRodgers7 <103163834+BryceRodgers7@users.noreply.github.com>
2024-06-20 18:39:16 -0500

    fix streamlit secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,12 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot - THE OMEGA TEST (By Bry
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot\naka THE OMEGA TEST\n(By Bryce Rodgers)')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
+        st.success('API key is good.', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+        st.warning('credentials are not working.', icon='⚠️')
 
-    st.subheader('Models and parameters')
+    st.subheader('Models and parameters')[[]]
     selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
     if selected_model == 'Llama2-7B':
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
@@ -51,7 +46,7 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+    output = replicate.run(llm, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
     return output
