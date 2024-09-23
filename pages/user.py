b0a99c726637ccadbe510e18c3631523c2435ebc
BryceRodgers7 <103163834+BryceRodgers7@users.noreply.github.com>
2024-09-23 18:31:44 -0500

    major changes, additional pages and navigation sidebar

---
+++ pages/user.py
@@ -0,0 +1,92 @@
+import streamlit as st
+from menu import menu_with_redirect
+
+# Redirect to app.py if not logged in, otherwise show the navigation menu
+menu_with_redirect()
+
+#st.title("This page is available to all users")
+
+
+import streamlit as st
+from menu import menu
+import replicate
+import os
+from random import randrange
+
+# App title
+#st.set_page_config(page_title="💬 Pirate Chatbot")
+st.title('💬 Pirate Chatbot\n(By Bryce Rodgers)')
+st.markdown(f"You are currently logged in with the role of {st.session_state.role}.")
+
+# Openers
+openers = ["Yaarg! Whachya need buckaroo?", "Arrr, ya need somethin'?", "Yarr matey got a question for me?"]
+
+# Replicate Credentials
+if 'REPLICATE_API_TOKEN' in st.secrets:
+    st.success('API key is good.', icon='✅')
+    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+else:
+    st.warning('credentials are not working.', icon='⚠️')
+
+st.subheader('Models and parameters')
+selected_model = st.selectbox('Choose a 🦙 Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Mistral-7B'], key='selected_model')
+if selected_model == 'Llama2-7B':
+    llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+elif selected_model == 'Llama2-13B':
+    llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+elif selected_model == 'Mistral-7B':
+    llm = 'mistralai/mistral-7b-instruct-v0.1:5fe0a3d7ac2852264a25279d1dfb798acbc4d49711d126646594e212cb821749'
+temperature = st.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+top_p = st.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+max_length = st.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": openers[randrange(2)]}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": openers[randrange(2)]}]
+st.button('Restart Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input, hiddenprompt):
+    if (hiddenprompt != None):
+        string_dialogue = hiddenprompt
+    else:
+        string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. You speak like a pirate."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+hidden_prompt = st.text_area("muck with the 'hidden' prompt below" , "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'. You talk like a pirate.")
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt, hidden_prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
