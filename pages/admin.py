fc837c25e09e0673730044aab4279c755fe4dfe5
BryceRodgers7 <103163834+BryceRodgers7@users.noreply.github.com>
2024-09-24 19:22:25 -0500

    set global variable type

--- pages/admin.py
+++ pages/admin.py
@@ -11,7 +11,7 @@ from random import randrange
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
 menu_with_redirect()
-current_content = None
+current_content = io.BytesIO()
 
 # Verify the user's role
 if st.session_state.role not in ["user", "admin", "super-admin"]:
@@ -65,7 +65,7 @@ def get_image(content):
     return current_content
 
 def get_image_bytes():
-    return current_content.getvalue()
+    return current_content
     
 if "show_pic" not in st.session_state:
         st.session_state.show_pic = False
@@ -112,10 +112,10 @@ def fake_hit_stab(prompt):
 #img_prompt = st.text_area("What would you like to see? RANDOM IMAGES ENABLED", "A beautiful parrot before a lush background of jungle canopy.")
 img_prompt = st.text_area("What would you like to see?", "A beautiful parrot before a lush background of jungle canopy.")
 click = st.button("See It!", help="submit your prompt and get an image", use_container_width=False)
-img_BufferedReader = io.BufferedReader(get_image_bytes()) #_io.BufferedReader
+img_BufferedReader = io.BufferedReader(get_image_bytes())
 btn = st.download_button(
       label="Download Image",
-      data=img_BufferedReader,
+      data=img_BufferedReader(),
       file_name="imagename.png",
       mime="image/jpeg",
       )
