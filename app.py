dcbb3a09e2a560b29dadf6ac124f9d993c3c61a1
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-10-20 00:18:21 -0500

    aaanother attempt to fix role bug

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import streamlit as st
 from menu import menu
 
-
 st.title("Data Science Demo Site")
 st.write("By Bryce Rodgers")
 menu() # Render the dynamic menu!
 
-st.session_state.role = "user"
+
+
+st.write("choose a role ffs")
+# Initialize st.session_state.role to None
+if "role" not in st.session_state:
+    st.session_state.role = None
+# Retrieve the role from Session State to initialize the widget
+st.session_state._role = st.session_state.role
+
+def set_role():
+    # Callback function to save the role selection to Session State
+    st.session_state.role = st.session_state._role
+
+# Selectbox to choose role
+st.selectbox(
+    "Select your role:",
+    [None, "user", "admin", "super-admin"],
+    key="_role",
+    on_change=set_role,
+)
+
 
 
 st.subheader("Navigate using the sidebar, or the links below")
