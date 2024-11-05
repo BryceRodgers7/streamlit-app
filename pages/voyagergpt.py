5844ca7ac720ca330b0f77046229fe9dd10cc2f9
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-05 00:41:23 -0600

    new context every button press

--- pages/voyagergpt.py
+++ pages/voyagergpt.py
@@ -189,14 +189,15 @@ model.eval()
 st.write("VoyagerGPT has over 10M parameters. Below are the chars in its vocabulary:")
 st.write("! # & ' ( ) + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < ? A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] _ a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ® � (and 'space' and 'return')")
 st.divider()
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
 
 st.sidebar.title('VoyagerGPT Panel')
 input_seed = st.sidebar.number_input("seed number", step=1, value=1337)
 
 if st.button("Generate some Star Trek text!!"):
-    start_time = time.time()
     torch.manual_seed(input_seed)
+    st.write(f"this will take a moment... using seed {input_seed}")
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    start_time = time.time()    
     strang = decode(model.generate(context, max_new_tokens=100)[0].tolist())
     strang = '<p>' + strang.replace('\n', '<br>') + '</p>'
     st.markdown(strang, unsafe_allow_html=True)
