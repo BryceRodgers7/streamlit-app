fe3a8cf2d11dacd9cde431f416ebd232e35bb590
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-06 01:57:19 -0600

    no change

--- pages/warcraft.py
+++ pages/warcraft.py
@@ -15,10 +15,6 @@ def load_model():
 
 model = load_model()
 
-# def classify_image(img):
-#     pred,idx,probs = model.predict(img)
-#     return dict(zip(categories, map(float,probs)))
-
 st.title('Warcraft Factions')
 
 def display_images_side_by_side(image_paths, captions, width=150):  
@@ -36,7 +32,7 @@ captions = ["Footman", "Grunt", "Ghoul", "Night Elf Archer"]
 # Call the function with the images you want to display side-by-side
 display_images_side_by_side(image_paths, captions)
 
-uploaded_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader("Upload an image to determine if it resembles what you see on the roster", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
     # Display the uploaded image
@@ -52,29 +48,3 @@ if uploaded_file is not None:
     st.write(f"Confidence: {probs[pred_idx]:.4f}")
 else:
     st.write("Please upload an image to classify.")
-
-# st.write("TODO: add an upload button here to check if an image file is a Footie, Grunt, Ghoul, or NE Archer.")
-
-
-# Load your images
-# foot = Image.open("./.static/footman.jpg")
-# grunt = Image.open("./.static/grunt.jpg")
-# ghoul = Image.open("./.static/ghoul.jpg")
-# nearcher = Image.open("./.static/nearcher.jpg")
-
-
-
-
-
-
-# st.write("Human Footman")
-# st.image('./.static/footman.jpg')
-
-# st.write("Orc Grunt")
-# st.image('./.static/grunt.jpg')
-
-# st.write("Undead Ghoul")
-# st.image('./.static/ghoul.jpg')
-
-# st.write("Night Elf Archer")
-# st.image('./.static/nearcher.jpg')
