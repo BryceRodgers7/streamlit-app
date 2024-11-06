579db74ed1ce70d516be209ce54e57f25d921386
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-06 01:55:08 -0600

    add in warcraft roster images

--- pages/warcraft.py
+++ pages/warcraft.py
@@ -21,6 +21,21 @@ model = load_model()
 
 st.title('Warcraft Factions')
 
+def display_images_side_by_side(image_paths, captions, width=150):  
+    # Create columns based on the number of images
+    columns = st.columns(len(image_paths))
+    
+    # Display each image in its respective column, along with caption
+    for col, img, caption in zip(columns, image_paths, captions):
+        with col:
+            st.image(img, caption=caption, use_column_width=False, width=width)
+
+st.write("The Warcraft Roster")
+image_paths = ["./.static/footman.jpg", "./.static/grunt.jpg", "./.static/ghoul.jpg", "./.static/nearcher.jpg"]
+captions = ["Footman", "Grunt", "Ghoul", "Night Elf Archer"]
+# Call the function with the images you want to display side-by-side
+display_images_side_by_side(image_paths, captions)
+
 uploaded_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
@@ -39,6 +54,18 @@ else:
     st.write("Please upload an image to classify.")
 
 # st.write("TODO: add an upload button here to check if an image file is a Footie, Grunt, Ghoul, or NE Archer.")
+
+
+# Load your images
+# foot = Image.open("./.static/footman.jpg")
+# grunt = Image.open("./.static/grunt.jpg")
+# ghoul = Image.open("./.static/ghoul.jpg")
+# nearcher = Image.open("./.static/nearcher.jpg")
+
+
+
+
+
 
 # st.write("Human Footman")
 # st.image('./.static/footman.jpg')
