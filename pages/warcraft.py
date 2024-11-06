6b4393e3268e0f865d96dadf7d3a3f23205195d8
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-06 02:45:09 -0600

    more static text changes

--- pages/warcraft.py
+++ pages/warcraft.py
@@ -16,7 +16,8 @@ def load_model():
 model = load_model()
 
 st.title('Warcraft Factions')
-st.subheader('the roster')
+st.subheader('The Roster')
+st.write('in warcraft, each faction has a basic type of soldier that makes up the bulk of their army')
 
 def display_images_side_by_side(image_paths, captions, width=150):  
     # Create columns based on the number of images
