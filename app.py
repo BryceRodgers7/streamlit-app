796d52bc38bf0f21b5b753bceb0aae7f856b62e3
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-06 21:46:53 -0600

    static text changes

--- app.py
+++ app.py
@@ -29,20 +29,27 @@ def set_role():
 
 st.markdown("<p style='font-size:20px; font-weight:bold;'>Navigate using the sidebar, or the links below</p>", unsafe_allow_html=True)
 
-st.page_link("pages/aboutme.py", label="About Me")
+
 
 st.divider()
 # st.write("Navigate using the sidebar, or the links below")
+st.page_link("pages/aboutme.py", label="About Me")
+st.write("a quick blurb about me, and what I'm doing with this website!")
 
-st.write("This Llama-2 Chatbot thinks he's a pirate!")
 st.page_link("pages/chatbot.py", label="Pirate Chatbot")
-st.write("Tell StabilityAI to make a picture for you!")
+st.write("This Llama-2 Chatbot thinks he's a pirate!")
+
 st.page_link("pages/stability.py", label="Picture Page")
-st.write("Generate a Star Trek script using VoyagerGPT!")
+st.write("Tell StabilityAI to make a picture for you!")
+
 st.page_link("pages/voyagergpt.py", label="VoyagerGPT")
-st.write("Familiarize yourself with different warriors in the world of Warcraft!")
+st.write("Generate a Star Trek script using VoyagerGPT!")
+
 st.page_link("pages/warcraft.py", label="Warcraft World")
-st.write("Discover the Dune Universe like you've never experienced it before!... A Topic Model using TF-IDF.")
+st.write("Familiarize yourself with different warriors in the world of Warcraft!")
+
 st.page_link("pages/dune.py", label="Dune Universe")
-st.write("Check out the Dune (2021) script and compare the topics against the books!")
+st.write("Discover the Dune Universe like you've never experienced it before!... A Topic Model using TF-IDF.")
+
 st.page_link("pages/pps.py", label="Dune Movie")
+st.write("Check out the Dune (2021) script and compare the topics against the books!")
