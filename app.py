badf7835e903cdc0a299d5f606ef30dd8569cb82
BryceRodgers7 <bryce.rodgers7@gmail.com>
2024-11-12 22:43:54 -0600

    static text updates

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from menu import menu
 
 st.title("Bryce Rodgers")
 st.subheader('Data Science Demo Site!')
-
+st.divider()
 if "role" not in st.session_state:
     st.session_state.role = 'user'
 # Retrieve the role from Session State to initialize the widget
@@ -27,27 +27,30 @@ def set_role():
 #     on_change=set_role,
 # )
 
+# use one of these 3
+st.write("Welcome! Explore this website by using the sidebar, or the links below")
+st.markdown("<p style='font-size:20px; font-weight:bold;'>Welcome! Explore this website by using the sidebar, or the links below</p>", unsafe_allow_html=True)
 st.markdown("<p style='font-size:20px; font-weight:bold;'>Navigate using the sidebar, or the links below</p>", unsafe_allow_html=True)
 
 st.divider()
 # st.write("Navigate using the sidebar, or the links below")
 st.page_link("pages/aboutme.py", label="About Me")
-st.write("A quick blurb about me, and what I'm doing with this website!")
-
+st.write("A quick blurb about me and this website.")
+st.divider()
 st.page_link("pages/chatbot.py", label="Pirate Chatbot")
 st.write("This Llama-2 Chatbot thinks he's a pirate!")
-
+st.divider()
 st.page_link("pages/stability.py", label="Picture Page")
 st.write("Tell StabilityAI to make a picture for you!")
-
+st.divider()
 st.page_link("pages/voyagergpt.py", label="VoyagerGPT")
 st.write("Generate a Star Trek script using my very own VoyagerGPT!")
-
+st.divider()
 st.page_link("pages/warcraft.py", label="Warcraft World")
 st.write("Familiarize yourself with different warriors from the World of Warcraft!")
-
+st.divider()
 st.page_link("pages/dune.py", label="Dune Universe")
 st.write("Discover the Dune Universe like you've never experienced it before!... A Topic Model using TF-IDF.")
-
+st.divider()
 st.page_link("pages/pps.py", label="Dune Movie")
 st.write("Check out the Dune (2021) script and compare the topics against the books!")
